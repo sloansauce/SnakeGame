023a40669fc87d4044257b0e058718338a0638b3
Kevin <ksloan90@gmail.com>
2025-09-20 01:25:18 -0400

    finish first iteration

--- Apple.py
+++ Apple.py
@@ -1,14 +1,14 @@
 import pygame
 from pygame import Vector2
 from random import randint
-from Settings import CELL_SIZE,CELL_NUMBER,RED
+from Settings import CELL_SIZE, CELL_NUMBER, SCOREBOARD_HEIGHT
 
 
 class APPLE:
     def __init__(self):
         # create an x and y
         self.x = randint(0, CELL_NUMBER-1)
-        self.y = randint(0, CELL_NUMBER-1)
+        self.y = randint(SCOREBOARD_HEIGHT//CELL_SIZE, CELL_NUMBER+ SCOREBOARD_HEIGHT//CELL_SIZE - 1)
         self.pos = Vector2(self.x, self.y)
         self.apple = pygame.image.load('assets/Graphics/apple.png').convert_alpha()
 
@@ -23,6 +23,6 @@ class APPLE:
         screen.blit(self.apple, apple_rect)
 
     def randomize(self):
-        self.x = randint(0, CELL_NUMBER - 1)
-        self.y = randint(0, CELL_NUMBER - 1)
+        self.x = randint(0, CELL_NUMBER-1)
+        self.y = randint(SCOREBOARD_HEIGHT//CELL_SIZE, CELL_NUMBER + SCOREBOARD_HEIGHT//CELL_SIZE - 1)
         self.pos = Vector2(self.x, self.y)
--- Snake.py
+++ Snake.py
@@ -4,7 +4,7 @@ from Settings import CELL_SIZE,CELL_NUMBER,WHITE
 
 class SNAKE:
     def __init__(self):
-        self.body = [Vector2(4, 10), Vector2(3, 10), Vector2(2, 10), Vector2(1, 10)]
+        self.body = [Vector2(4, 10), Vector2(3, 10), Vector2(2, 10)]
         self.direction = Vector2(1, 0)
 
         self.head_up = pygame.image.load('assets/Graphics/head_up.png')
@@ -25,6 +25,8 @@ class SNAKE:
         self.body_br = pygame.image.load('assets/Graphics/body_br.png')
         self.body_bl = pygame.image.load('assets/Graphics/body_bl.png')
 
+        self.crunch_sound = pygame.mixer.Sound('assets/Sound/crunch.wav')
+
     def draw_snake(self,screen):
         self.update_head_graphics()
         self.update_tail_graphics()
@@ -41,12 +43,20 @@ class SNAKE:
                 screen.blit(self.tail,block_rect)
             else:
                 previous_block = self.body[index+1] - block
-                next_block = self.body[index] - block
+                next_block = self.body[index-1] - block
                 if previous_block.x == next_block.x:
                     screen.blit(self.body_vertical, block_rect)
                 elif previous_block.y == next_block.y:
                     screen.blit(self.body_horizontal, block_rect)
-
+                else:
+                    if (previous_block.x == -1 and next_block.y == -1) or (previous_block.y == -1 and next_block.x == -1):
+                        screen.blit(self.body_tl, block_rect)
+                    elif (previous_block.x == 1 and next_block.y == 1) or (previous_block.y == 1 and next_block.x == 1):
+                        screen.blit(self.body_br, block_rect)
+                    elif (previous_block.x == -1 and next_block.y == 1) or (previous_block.y == 1 and next_block.x == -1):
+                        screen.blit(self.body_bl, block_rect)
+                    elif (previous_block.x == 1 and next_block.y == -1) or (previous_block.y == -1 and next_block.x == 1):
+                        screen.blit(self.body_tr, block_rect)
 
     def update_head_graphics(self):
         head_direction = self.body[1] - self.body[0]
@@ -71,3 +81,6 @@ class SNAKE:
         body_copy = self.body[:]
         body_copy.insert(0, body_copy[0] + self.direction)
         self.body = body_copy
+
+    def play_crunch_sound(self):
+        self.crunch_sound.play()
